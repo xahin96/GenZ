692287eca34001e7d0acc903558e86003b49748a
Nana <nanasmacbookprowithtouchbar@songruohandeMacBook-Pro.local>
2024-06-05 20:28:06 -0400

    Ruohan|home page created

--- GenZ/GenZ/settings.py
+++ GenZ/GenZ/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "Employee.apps.EmployeeConfig"
+    "Employee.apps.EmployeeConfig",
+    "main.apps.MainConfig",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = "GenZ.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
