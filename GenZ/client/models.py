11e51f385c8221b397bf36f24c3e288440b55483
sachreetkaur <sachreetkaur112@gmail.com>
2024-06-23 18:26:55 -0400

    client dashboard

--- GenZ/client/models.py
+++ GenZ/client/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Content(models.Model):
+    content = models.TextField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True,default=timezone.now)
+
+    def __str__(self):
+        return self.content
+class Question(models.Model):
+    question = models.TextField(null=True, blank=True)
+    date = models.DateField(null=True, blank=True,default=timezone.now)
+    content = models.ForeignKey(Content, null=True, blank=True,  on_delete=models.CASCADE, related_name='questions')
+    answer = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.question
+
+
--- GenZ/client/views.py
+++ GenZ/client/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import *
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    contents = Content.objects.all().order_by('-pk')
+    return render(request, 'index.html', {'contents': contents})
+
+
+def submit(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        data = Content.objects.create(content=content)
+        data.save()
+    contents = Content.objects.all().order_by('-pk')
+    return render(request, 'index.html', {'contents': contents})
