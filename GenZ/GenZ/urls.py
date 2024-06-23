5a6f2a79ce16dc585bdc61c5353d195f4f27b68f
kashyap-prajapati <prajapatikashyap1997@gmail.com>
2024-06-23 12:43:50 -0400

    client design client

--- GenZ/GenZ/urls.py
+++ GenZ/GenZ/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("Employee/", include("Employee.urls")),
+    path("client/",include("client.urls")),
 ]
