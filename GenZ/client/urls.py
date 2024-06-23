86419c05e83eafbdc6b16de78c6cdfc2ca1b1c8e
sachreetkaur <sachreetkaur112@gmail.com>
2024-06-23 18:26:55 -0400

    client dashboard

--- GenZ/client/urls.py
+++ GenZ/client/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . views import *
 
 urlpatterns = [
-    path("",index,name="home")
+    path("",index,name="home"),
+    path("submit",submit,name="submit")
 ]
