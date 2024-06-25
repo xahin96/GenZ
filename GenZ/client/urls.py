aba8e0dcb2d676ecc634451b99888835b64a5477
sachreetkaur <sachreetkaur112@gmail.com>
2024-06-25 15:49:25 -0400

    client dashboard

--- GenZ/client/urls.py
+++ GenZ/client/urls.py
@@ -4,5 +4,7 @@ from . views import *
 
 urlpatterns = [
     path("",index,name="home"),
-    path("submit",submit,name="submit")
+    path("submit",submit,name="submit"),
+    path("<int:content_id>/",content,name="content"),
+    path("<int:content_id>/submit", question, name="question"),
 ]
